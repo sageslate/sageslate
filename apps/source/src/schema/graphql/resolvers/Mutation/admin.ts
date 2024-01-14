6c681dd4140358c1b2658bc44669df715d47d5ca
Nicolai Moraru <ceo@nickmessing.com>
2024-01-14 14:22:56 +0200

    feat(core): implement dev mode for graphql generation

--- apps/source/src/schema/graphql/resolvers/Mutation/admin.ts
+++ apps/source/src/schema/graphql/resolvers/Mutation/admin.ts
@@ -1,4 +1,4 @@
 import type { MutationResolvers } from './../../../types.generated'
-export const admin: NonNullable<MutationResolvers['admin']> = async (_parent, _argument, _context) => {
+export const admin: NonNullable<MutationResolvers['admin']> = (_parent, _argument, _context) => {
   return {}
 }
--- codegen.ts
+++ codegen.ts
@@ -1,14 +1,12 @@
-/// <reference types="@graphql-codegen/plugin-helpers" />
-
 import { defineConfig } from '@eddeee888/gcg-typescript-resolver-files'
 
 import type { CodegenConfig } from '@graphql-codegen/cli'
 
 // eslint-disable-next-line import/no-default-export
 export default {
-  schema: 'src/graphql/*.graphql',
+  schema: 'apps/source/src/graphql/**/*.graphql',
   generates: {
-    'src/schema': defineConfig({
+    'apps/source/src/schema': defineConfig({
       resolverGeneration: 'all',
     }),
   },
